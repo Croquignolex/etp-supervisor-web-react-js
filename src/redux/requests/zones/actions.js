67dabdefad1e631f763288ff6b0f62b39317a38e
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2021-02-23 16:32:16 +0100

    Redux request data

--- src/redux/requests/zones/actions.js
+++ src/redux/requests/zones/actions.js
@@ -1,9 +1,88 @@
 // Reducer action types
+export const STORE_ZONES_REQUEST_INIT = 'STORE_ZONES_REQUEST_INIT';
+export const STORE_ZONES_REQUEST_RESET = 'STORE_ZONES_REQUEST_RESET';
+export const STORE_ZONES_REQUEST_FAILED = 'STORE_ZONES_REQUEST_FAILED';
+export const STORE_ZONES_REQUEST_SUCCEEDED = 'STORE_ZONES_REQUEST_SUCCEEDED';
+
+export const STORE_NEXT_ZONES_REQUEST_INIT = 'STORE_NEXT_ZONES_REQUEST_INIT';
+export const STORE_NEXT_ZONES_REQUEST_RESET = 'STORE_NEXT_ZONES_REQUEST_RESET';
+export const STORE_NEXT_ZONES_REQUEST_FAILED = 'STORE_NEXT_ZONES_REQUEST_FAILED';
+export const STORE_NEXT_ZONES_REQUEST_SUCCEEDED = 'STORE_NEXT_ZONES_REQUEST_SUCCEEDED';
+
 export const STORE_ALL_ZONES_REQUEST_INIT = 'STORE_ALL_ZONES_REQUEST_INIT';
 export const STORE_ALL_ZONES_REQUEST_RESET = 'STORE_ALL_ZONES_REQUEST_RESET';
 export const STORE_ALL_ZONES_REQUEST_FAILED = 'STORE_ALL_ZONES_REQUEST_FAILED';
 export const STORE_ALL_ZONES_REQUEST_SUCCEEDED = 'STORE_ALL_ZONES_REQUEST_SUCCEEDED';
 
+export const STORE_ADD_ZONE_REQUEST_INIT = 'STORE_ADD_ZONE_REQUEST_INIT';
+export const STORE_ADD_ZONE_REQUEST_RESET = 'STORE_ADD_ZONE_REQUEST_RESET';
+export const STORE_ADD_ZONE_REQUEST_FAILED = 'STORE_ADD_ZONE_REQUEST_FAILED';
+export const STORE_ADD_ZONE_REQUEST_SUCCEEDED = 'STORE_ADD_ZONE_REQUEST_SUCCEEDED';
+
+export const STORE_SHOW_ZONE_REQUEST_INIT = 'STORE_SHOW_ZONE_REQUEST_INIT';
+export const STORE_SHOW_ZONE_REQUEST_RESET = 'STORE_SHOW_ZONE_REQUEST_RESET';
+export const STORE_SHOW_ZONE_REQUEST_FAILED = 'STORE_SHOW_ZONE_REQUEST_FAILED';
+export const STORE_SHOW_ZONE_REQUEST_SUCCEEDED = 'STORE_SHOW_ZONE_REQUEST_SUCCEEDED';
+
+export const STORE_EDIT_ZONE_REQUEST_INIT = 'STORE_EDIT_ZONE_REQUEST_INIT';
+export const STORE_EDIT_ZONE_REQUEST_RESET = 'STORE_EDIT_ZONE_REQUEST_RESET';
+export const STORE_EDIT_ZONE_REQUEST_FAILED = 'STORE_EDIT_ZONE_REQUEST_FAILED';
+export const STORE_EDIT_ZONE_REQUEST_SUCCEEDED = 'STORE_EDIT_ZONE_REQUEST_SUCCEEDED';
+
+export const STORE_ZONE_EDIT_COLLECTOR_REQUEST_INIT = 'STORE_ZONE_EDIT_COLLECTOR_REQUEST_INIT';
+export const STORE_ZONE_EDIT_COLLECTOR_REQUEST_RESET = 'STORE_ZONE_EDIT_COLLECTOR_REQUEST_RESET';
+export const STORE_ZONE_EDIT_COLLECTOR_REQUEST_FAILED = 'STORE_ZONE_EDIT_COLLECTOR_REQUEST_FAILED';
+export const STORE_ZONE_EDIT_COLLECTOR_REQUEST_SUCCEEDED = 'STORE_ZONE_EDIT_COLLECTOR_REQUEST_SUCCEEDED';
+
+export const STORE_ZONE_ADD_AGENT_REQUEST_INIT = 'STORE_ZONE_ADD_AGENT_REQUEST_INIT';
+export const STORE_ZONE_ADD_AGENT_REQUEST_RESET = 'STORE_ZONE_ADD_AGENT_REQUEST_RESET';
+export const STORE_ZONE_ADD_AGENT_REQUEST_FAILED = 'STORE_ZONE_ADD_AGENT_REQUEST_FAILED';
+export const STORE_ZONE_ADD_AGENT_REQUEST_SUCCEEDED = 'STORE_ZONE_ADD_AGENT_REQUEST_SUCCEEDED';
+
+// ======================================================== Zones
+// Set zones init data into store
+export const storeZonesRequestInit = () => ({
+    type: STORE_ZONES_REQUEST_INIT
+});
+
+// Set zones failed data into store
+export const storeZonesRequestFailed = ({message}) => ({
+    message,
+    type: STORE_ZONES_REQUEST_FAILED
+});
+
+// Set zones succeeded data into store
+export const storeZonesRequestSucceed = ({message}) => ({
+    message,
+    type: STORE_ZONES_REQUEST_SUCCEEDED
+});
+
+// Set zones reset data into store
+export const storeZonesRequestReset = () => ({
+    type: STORE_ZONES_REQUEST_RESET
+});
+// ======================================================== Next zones
+// Set next zones init data into store
+export const storeNextZonesRequestInit = () => ({
+    type: STORE_NEXT_ZONES_REQUEST_INIT
+});
+
+// Set next zones failed data into store
+export const storeNextZonesRequestFailed = ({message}) => ({
+    message,
+    type: STORE_NEXT_ZONES_REQUEST_FAILED
+});
+
+// Set next zones succeeded data into store
+export const storeNextZonesRequestSucceed = ({message}) => ({
+    message,
+    type: STORE_NEXT_ZONES_REQUEST_SUCCEEDED
+});
+
+// Set next zones reset data into store
+export const storeNextZonesRequestReset = () => ({
+    type: STORE_NEXT_ZONES_REQUEST_RESET
+});
 // ======================================================== All zones
 // Set all zones init data into store
 export const storeAllZonesRequestInit = () => ({
@@ -25,4 +104,114 @@ export const storeAllZonesRequestSucceed = ({message}) => ({
 // Set all zones reset data into store
 export const storeAllZonesRequestReset = () => ({
     type: STORE_ALL_ZONES_REQUEST_RESET
+});
+// ======================================================== Add zone
+// Set add zone init data into store
+export const storeAddZoneRequestInit = () => ({
+    type: STORE_ADD_ZONE_REQUEST_INIT
+});
+
+// Set add zone failed data into store
+export const storeAddZoneRequestFailed = ({message}) => ({
+    message,
+    type: STORE_ADD_ZONE_REQUEST_FAILED
+});
+
+// Set add zone succeeded data into store
+export const storeAddZoneRequestSucceed = ({message}) => ({
+    message,
+    type: STORE_ADD_ZONE_REQUEST_SUCCEEDED
+});
+
+// Set add zone reset data into store
+export const storeAddZoneRequestReset = () => ({
+    type: STORE_ADD_ZONE_REQUEST_RESET
+});
+// ======================================================== Show zone
+// Set show zone init data into store
+export const storeShowZoneRequestInit = () => ({
+    type: STORE_SHOW_ZONE_REQUEST_INIT
+});
+
+// Set show zone failed data into store
+export const storeShowZoneRequestFailed = ({message}) => ({
+    message,
+    type: STORE_SHOW_ZONE_REQUEST_FAILED
+});
+
+// Set show zone succeeded data into store
+export const storeShowZoneRequestSucceed = ({message}) => ({
+    message,
+    type: STORE_SHOW_ZONE_REQUEST_SUCCEEDED
+});
+
+// Set show zone reset data into store
+export const storeShowZoneRequestReset = () => ({
+    type: STORE_SHOW_ZONE_REQUEST_RESET
+});
+// ======================================================== Edit zone
+// Set edit zone init data into store
+export const storeEditZoneRequestInit = () => ({
+    type: STORE_EDIT_ZONE_REQUEST_INIT
+});
+
+// Set edit zone failed data into store
+export const storeEditZoneRequestFailed = ({message}) => ({
+    message,
+    type: STORE_EDIT_ZONE_REQUEST_FAILED
+});
+
+// Set edit zone succeeded data into store
+export const storeEditZoneRequestSucceed = ({message}) => ({
+    message,
+    type: STORE_EDIT_ZONE_REQUEST_SUCCEEDED
+});
+
+// Set edit zone reset data into store
+export const storeEditZoneRequestReset = () => ({
+    type: STORE_EDIT_ZONE_REQUEST_RESET
+});
+// ======================================================== Zone add agent
+// Set zone add agent init data into store
+export const storeZoneAddSAgentRequestInit = () => ({
+    type: STORE_ZONE_ADD_AGENT_REQUEST_INIT
+});
+
+// Set zone add agent failed data into store
+export const storeZoneAddAgentRequestFailed = ({message}) => ({
+    message,
+    type: STORE_ZONE_ADD_AGENT_REQUEST_FAILED
+});
+
+// Set zone add agent succeeded data into store
+export const storeZoneAddAgentRequestSucceed = ({message}) => ({
+    message,
+    type: STORE_ZONE_ADD_AGENT_REQUEST_SUCCEEDED
+});
+
+// Set zone add agent reset data into store
+export const storeZoneAddAgentRequestReset = () => ({
+    type: STORE_ZONE_ADD_AGENT_REQUEST_RESET
+});
+// ======================================================== Zone edit collector
+// Set zone edit collector init data into store
+export const storeZoneEditCollectorRequestInit = () => ({
+    type: STORE_ZONE_EDIT_COLLECTOR_REQUEST_INIT
+});
+
+// Set zone edit collector failed data into store
+export const storeZoneEditCollectorRequestFailed = ({message}) => ({
+    message,
+    type: STORE_ZONE_EDIT_COLLECTOR_REQUEST_FAILED
+});
+
+// Set zone edit collector succeeded data into store
+export const storeZoneEditCollectorRequestSucceed = ({message}) => ({
+    message,
+    type: STORE_ZONE_EDIT_COLLECTOR_REQUEST_SUCCEEDED
+});
+
+// Set zone edit collector reset data into store
+export const storeZoneEditCollectorRequestReset = () => ({
+    type: STORE_ZONE_EDIT_COLLECTOR_REQUEST_RESET
 });
--- src/redux/requests/zones/reducer.js
+++ src/redux/requests/zones/reducer.js
@@ -3,13 +3,56 @@ import {requestFailedValue, requestInitValue, requestSucceededValue} from "../..
 
 // Partial global store for requests data management
 const initialState = {
-    all: {failed: false, loading: false, succeeded: false, message: ""}
+    all: {failed: false, loading: false, succeeded: false, message: ""},
+    add: {failed: false, loading: false, succeeded: false, message: ""},
+    list: {failed: false, loading: false, succeeded: false, message: ""},
+    next: {failed: false, loading: false, succeeded: false, message: ""},
+    show: {failed: false, loading: false, succeeded: false, message: ""},
+    edit: {
+        info: {failed: false, loading: false, succeeded: false, message: ""},
+        agent: {failed: false, loading: false, succeeded: false, message: ""},
+        collector: {failed: false, loading: false, succeeded: false, message: ""},
+    }
 };
 
 // Reduce
 function reduce(state = initialState, action) {
     let nextState;
     switch (action.type) {
+        // ======================================================== Zones
+        // Resolve event to set zones init request store data
+        case actions.STORE_ZONES_REQUEST_INIT:
+            nextState = {...state, list: requestInitValue()};
+            return nextState || state;
+        // Resolve event to set zones failed request store data
+        case actions.STORE_ZONES_REQUEST_FAILED:
+            nextState = {...state, list: requestFailedValue(action.message)};
+            return nextState || state;
+        // Resolve event to set zones succeeded request store data
+        case actions.STORE_ZONES_REQUEST_SUCCEEDED:
+            nextState = {...state, list: requestSucceededValue(action.message)};
+            return nextState || state;
+        // Resolve event to set zones reset request store data
+        case actions.STORE_ZONES_REQUEST_RESET:
+            nextState = {...state, list: initialState.list};
+            return nextState || state;
+        // ======================================================== Next zones
+        // Resolve event to set next zones init request store data
+        case actions.STORE_NEXT_ZONES_REQUEST_INIT:
+            nextState = {...state, next: requestInitValue()};
+            return nextState || state;
+        // Resolve event to set next zones failed request store data
+        case actions.STORE_NEXT_ZONES_REQUEST_FAILED:
+            nextState = {...state, next: requestFailedValue(action.message)};
+            return nextState || state;
+        // Resolve event to set next zones succeeded request store data
+        case actions.STORE_NEXT_ZONES_REQUEST_SUCCEEDED:
+            nextState = {...state, next: requestSucceededValue(action.message)};
+            return nextState || state;
+        // Resolve event to set next zones reset request store data
+        case actions.STORE_NEXT_ZONES_REQUEST_RESET:
+            nextState = {...state, next: initialState.next};
+            return nextState || state;
         // ======================================================== All zones
         // Resolve event to set all zones  init request store data
         case actions.STORE_ALL_ZONES_REQUEST_INIT:
@@ -26,6 +69,91 @@ function reduce(state = initialState, action) {
         // Resolve event to set all zones reset request store data
         case actions.STORE_ALL_ZONES_REQUEST_RESET:
             nextState = {...state, all: initialState.all};
+            return nextState || state;
+        // ======================================================== Add zone
+        // Resolve event to set add zone init request store data
+        case actions.STORE_ADD_ZONE_REQUEST_INIT:
+            nextState = {...state, add: requestInitValue()};
+            return nextState || state;
+        // Resolve event to set add zone failed request store data
+        case actions.STORE_ADD_ZONE_REQUEST_FAILED:
+            nextState = {...state, add: requestFailedValue(action.message)};
+            return nextState || state;
+        // Resolve event to set add zone succeeded request store data
+        case actions.STORE_ADD_ZONE_REQUEST_SUCCEEDED:
+            nextState = {...state, add: requestSucceededValue(action.message)};
+            return nextState || state;
+        // Resolve event to set add zone reset request store data
+        case actions.STORE_ADD_ZONE_REQUEST_RESET:
+            nextState = {...state, add: initialState.add};
+            return nextState || state;
+        // ======================================================== Show zone
+        // Resolve event to set show zone init request store data
+        case actions.STORE_SHOW_ZONE_REQUEST_INIT:
+            nextState = {...state, show: requestInitValue()};
+            return nextState || state;
+        // Resolve event to set show zone failed request store data
+        case actions.STORE_SHOW_ZONE_REQUEST_FAILED:
+            nextState = {...state, show: requestFailedValue(action.message)};
+            return nextState || state;
+        // Resolve event to set show zone succeeded request store data
+        case actions.STORE_SHOW_ZONE_REQUEST_SUCCEEDED:
+            nextState = {...state, show: requestSucceededValue(action.message)};
+            return nextState || state;
+        // Resolve event to set show zone reset request store data
+        case actions.STORE_SHOW_ZONE_REQUEST_RESET:
+            nextState = {...state, show: initialState.show};
+            return nextState || state;
+        // ======================================================== Edit zone
+        // Resolve event to set edit zone init request store data
+        case actions.STORE_EDIT_ZONE_REQUEST_INIT:
+            nextState = {...state, edit: {...state.edit, info: requestInitValue()}};
+            return nextState || state;
+        // Resolve event to set edit zone failed request store data
+        case actions.STORE_EDIT_ZONE_REQUEST_FAILED:
+            nextState = {...state, edit: {...state.edit, info: requestFailedValue(action.message)}};
+            return nextState || state;
+        // Resolve event to set edit zone succeeded request store data
+        case actions.STORE_EDIT_ZONE_REQUEST_SUCCEEDED:
+            nextState = {...state, edit: {...state.edit, info: requestSucceededValue(action.message)}};
+            return nextState || state;
+        // Resolve event to set edit zone reset request store data
+        case actions.STORE_EDIT_ZONE_REQUEST_RESET:
+            nextState = {...state, edit: {...state.edit, info: initialState.edit.info}};
+            return nextState || state;
+        // ======================================================== Zone add agent
+        // Resolve event to set zone add agent init request store data
+        case actions.STORE_ZONE_ADD_AGENT_REQUEST_INIT:
+            nextState = {...state, edit: {...state.edit, agent: requestInitValue()}};
+            return nextState || state;
+        // Resolve event to set zone add agent failed request store data
+        case actions.STORE_ZONE_ADD_AGENT_REQUEST_FAILED:
+            nextState = {...state, edit: {...state.edit, agent: requestFailedValue(action.message)}};
+            return nextState || state;
+        // Resolve event to set zone add agent succeeded request store data
+        case actions.STORE_ZONE_ADD_AGENT_REQUEST_SUCCEEDED:
+            nextState = {...state, edit: {...state.edit, agent: requestSucceededValue(action.message)}};
+            return nextState || state;
+        // Resolve event to set zone add agent reset request store data
+        case actions.STORE_ZONE_ADD_AGENT_REQUEST_RESET:
+            nextState = {...state, edit: {...state.edit, agent: initialState.edit.agent}};
+            return nextState || state;
+        // ======================================================== Zone edit collector
+        // Resolve event to set zone edit collector init request store data
+        case actions.STORE_ZONE_EDIT_COLLECTOR_REQUEST_INIT:
+            nextState = {...state, edit: {...state.edit, collector: requestInitValue()}};
+            return nextState || state;
+        // Resolve event to set zone edit collector failed request store data
+        case actions.STORE_ZONE_EDIT_COLLECTOR_REQUEST_FAILED:
+            nextState = {...state, edit: {...state.edit, collector: requestFailedValue(action.message)}};
+            return nextState || state;
+        // Resolve event to set zone edit collector succeeded request store data
+        case actions.STORE_ZONE_EDIT_COLLECTOR_REQUEST_SUCCEEDED:
+            nextState = {...state, edit: {...state.edit, collector: requestSucceededValue(action.message)}};
+            return nextState || state;
+        // Resolve event to set zone edit collector reset request store data
+        case actions.STORE_ZONE_EDIT_COLLECTOR_REQUEST_RESET:
+            nextState = {...state, edit: {...state.edit, collector: initialState.edit.collector}};
             return nextState || state;
         // ========================================================
         // Unknown action
