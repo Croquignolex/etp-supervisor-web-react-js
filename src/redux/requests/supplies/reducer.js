ece5c6b919cfbad65c4b1b3b9f60bb8e845e0afc
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2021-03-24 13:24:45 +0100

    Save

--- src/redux/requests/supplies/reducer.js
+++ src/redux/requests/supplies/reducer.js
@@ -5,6 +5,7 @@ import {requestFailedValue, requestInitValue, requestSucceededValue} from "../..
 const initialState = {
     list: {failed: false, loading: false, succeeded: false, message: ""},
     next: {failed: false, loading: false, succeeded: false, message: ""},
+    show: {failed: false, loading: false, succeeded: false, message: ""},
 };
 
 // Reduce
@@ -44,6 +45,23 @@ function reduce(state = initialState, action) {
         // Resolve event to set next supplies reset request store data
         case actions.STORE_NEXT_SUPPLIES_REQUEST_RESET:
             nextState = {...state, next: initialState.next};
+            return nextState || state;
+        // ======================================================== Show supply
+        // Resolve event to set show supply init request store data
+        case actions.STORE_SHOW_SUPPLY_REQUEST_INIT:
+            nextState = {...state, show: requestInitValue()};
+            return nextState || state;
+        // Resolve event to set show supply failed request store data
+        case actions.STORE_SHOW_SUPPLY_REQUEST_FAILED:
+            nextState = {...state, show: requestFailedValue(action.message)};
+            return nextState || state;
+        // Resolve event to set show supply succeeded request store data
+        case actions.STORE_SHOW_SUPPLY_REQUEST_SUCCEEDED:
+            nextState = {...state, show: requestSucceededValue(action.message)};
+            return nextState || state;
+        // Resolve event to set show supply reset request store data
+        case actions.STORE_SHOW_SUPPLY_REQUEST_RESET:
+            nextState = {...state, show: initialState.next};
             return nextState || state;
         // ========================================================
         // Unknown action
--- src/redux/supplies/actions.js
+++ src/redux/supplies/actions.js
@@ -1,13 +1,22 @@
 // Reducer action types
+export const STORE_SET_SUPPLY_DATA = 'STORE_SET_SUPPLY_DATA';
 export const STORE_SET_SUPPLIES_DATA = 'STORE_SET_SUPPLIES_DATA';
 export const STORE_SET_NEXT_SUPPLIES_DATA = 'STORE_SET_NEXT_SUPPLIES_DATA';
 export const STORE_STOP_INFINITE_SCROLL_SUPPLY_DATA = 'STORE_STOP_INFINITE_SCROLL_SUPPLY_DATA';
 
 // Middleware action types
+export const EMIT_SUPPLY_FETCH = 'EMIT_SUPPLY_FETCH';
 export const EMIT_SUPPLIES_FETCH = 'EMIT_SUPPLIES_FETCH';
 export const EMIT_NEXT_SUPPLIES_FETCH = 'EMIT_NEXT_SUPPLIES_FETCH';
 
 //====================== Reducer trigger actions
+// Set agent data in store
+export const storeSetSupplyData = ({supply, alsoInList = false}) => ({
+    supply,
+    alsoInList,
+    type: STORE_SET_SUPPLY_DATA
+});
+
 // Set supplies data in store
 export const storeSetSuppliesData = ({supplies, hasMoreData, page}) => ({
     page,
@@ -39,4 +48,10 @@ export const emitSuppliesFetch = () => ({
 export const emitNextSuppliesFetch = ({page}) => ({
     page,
     type: EMIT_NEXT_SUPPLIES_FETCH
+});
+
+// Emit supply fetch
+export const emitSupplyFetch = ({id}) => ({
+    id,
+    type: EMIT_SUPPLY_FETCH
 });
