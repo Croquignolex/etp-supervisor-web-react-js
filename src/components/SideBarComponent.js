8ee5825cc4cb4cf9353585e0475975e426b01045
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2021-06-01 11:26:13 +0100

    ok

--- src/components/SideBarComponent.js
+++ src/components/SideBarComponent.js
@@ -144,7 +144,7 @@ function buildOperationsMenu() {
         sub: [
             {name: page.OPERATIONS_TRANSFERS_PAGE, path: path.OPERATIONS_TRANSFERS_PAGE_PATH},
             {name: page.OPERATIONS_FLEETS_PAGE, path: path.OPERATIONS_FLEETS_PAGE_PATH},
-            // {name: page.OPERATIONS_CLEARANCES_PAGE, path: path.OPERATIONS_CLEARANCES_PAGE_PATH},
+            {name: page.OPERATIONS_CLEARANCES_PAGE, path: path.OPERATIONS_CLEARANCES_PAGE_PATH},
             // {name: page.OPERATIONS_AFFORDS_PAGE, path: path.OPERATION_AFFORDS_PAGE_PATH}
         ]
     }
