057d5669cd32c972d52d384debfee1163241233b
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2021-06-22 17:23:28 +0100

    Remote one

--- src/components/checkout/CheckoutExpensesCardsComponent.js
+++ src/components/checkout/CheckoutExpensesCardsComponent.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import PropTypes from "prop-types";
+import React, {useState} from 'react';
 
 import FormModalComponent from "../modals/FormModalComponent";
 import {fleetTypeBadgeColor} from "../../functions/typeFunctions";
