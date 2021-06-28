848faa8ae7b67afbdbc5df46948381b05b7e5834
Croquignolex <gpetitalex10@gmail.com>
2021-06-28 15:47:12 +0100

    Save excel export

--- src/components/managers/ManagerMovementsComponent.js
+++ src/components/managers/ManagerMovementsComponent.js
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
 import React, {useEffect, useState} from 'react';
 
+import ExcelFile from "react-data-export/dist/ExcelPlugin/components/ExcelFile";
+import ExcelSheet from "react-data-export/dist/ExcelPlugin/elements/ExcelSheet";
+import ExcelColumn from "react-data-export/dist/ExcelPlugin/elements/ExcelColumn";
+
 import LoaderComponent from "../LoaderComponent";
 import ErrorAlertComponent from "../ErrorAlertComponent";
 import DatePickerComponent from "../form/DatePickerComponent";
@@ -47,6 +51,16 @@ function ManagerMovementsComponent({manager, dispatch, request}) {
                             <button type="button" className="btn btn-theme mb-1 mr-1">
                                 <i className="fa fa-file-excel" /> Exporter en excel
                             </button>
+                            <ExcelFile element={<button>Download Data</button>}>
+                                <ExcelSheet data={manager.movements} name="movements">
+                                    <ExcelColumn label="DATE" value="creation"/>
+                                    <ExcelColumn label="TYPE" value="type"/>
+                                    <ExcelColumn label="NATURE" value="label"/>
+                                    <ExcelColumn label="ENTREES" value="in"/>
+                                    <ExcelColumn label="SORTIES" value="out"/>
+                                    <ExcelColumn label="SOLDES" value="balance"/>
+                                </ExcelSheet>
+                            </ExcelFile>
                             <DatePickerComponent input={selectedDate} handleInput={handleSelectedDate} />
                             <div className="card">
                                 <div className="table-responsive">
