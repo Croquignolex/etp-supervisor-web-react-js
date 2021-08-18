3d665ef4e5535e15856c68e55b62d9b820213bd9
Croquignolex <gpetitalex10@gmail.com>
2021-08-18 10:45:49 +0100

    Save

--- src/constants/generalConstants.js
+++ src/constants/generalConstants.js
@@ -1,12 +1,7 @@
-export const APP_NAME = 'MMAC';
-export const ENABLE_NOTIFICATION = true;
+// General
+export const APP_NAME = process.env.REACT_APP_APP_NAME;
 
-// Comment this before "npm build" execution for production
-export const AUTH_URL = 'http://localhost:3001';
-export const BASE_URL = 'http://localhost:3000';
-export const API_SERVER_URL = 'http://127.0.0.1';
-
-// Uncomment this before "npm build" execution for production
-// export const AUTH_URL = 'https://etp.moyo-industry.com';
-// export const BASE_URL = 'https://etp-supervisor.moyo-industry.com';
-// export const API_SERVER_URL = 'https://etp-api.moyo-industry.com/public';
+// Urls
+export const AUTH_URL = process.env.REACT_APP_AUTH_URL;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const API_SERVER_URL = process.env.REACT_APP_API_SERVER_URL;
