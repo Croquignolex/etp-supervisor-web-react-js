a9f9919a86261831cdee777d3bb8c4d0c7b69e0f
Croquignolex <gpetitalex10@gmail.com>
2022-01-11 10:23:22 +0100

    Fix supply detail

--- src/components/operations/SupplyInfoComponent.js
+++ src/components/operations/SupplyInfoComponent.js
@@ -2,18 +2,21 @@ import PropTypes from "prop-types";
 import React, {useState} from 'react';
 
 import OperatorComponent from "../OperatorComponent";
+import {AGENT_TYPE} from "../../constants/typeConstants";
 import FormModalComponent from "../modals/FormModalComponent";
 import {fleetTypeBadgeColor} from "../../functions/typeFunctions";
 import {dateToString, formatNumber} from "../../functions/generalFunctions";
 import SimDetailsContainer from "../../containers/sims/SimDetailsContainer";
 import AgentDetailsContainer from "../../containers/agents/AgentDetailsContainer";
 import ManagerDetailsContainer from "../../containers/managers/ManagerDetailsContainer";
+import ResourceDetailsContainer from "../../containers/resources/ResourceDetailsContainer";
 
 // Component
 function SupplyInfoComponent({supply}) {
     // Local states
     const [simDetailsModal, setSimDetailsModal] = useState({show: false, header: 'DETAIL DU COMPTE', id: ''});
-    const [agentDetailsModal, setAgentDetailsModal] = useState({show: false, header: "DETAIL DE L'AGENT/RESSOURCE", id: ''});
+    const [agentDetailsModal, setAgentDetailsModal] = useState({show: false, header: "DETAIL DE L'AGENT", id: ''});
+    const [resourceDetailsModal, setResourceDetailsModal] = useState({show: false, header: "DETAIL DE LA RESSOURCE", id: ''});
     const [managerDetailsModal, setManagerDetailsModal] = useState({show: false, header: "DETAIL DE LA GESTIONNAIRE", id: ''});
 
     // Hide agent details modal form
@@ -31,6 +34,11 @@ function SupplyInfoComponent({supply}) {
         setSimDetailsModal({...simDetailsModal, show: false})
     }
 
+    // Hide resource details modal form
+    const handleResourceDetailsModalHide = () => {
+        setResourceDetailsModal({...resourceDetailsModal, show: false})
+    }
+
     // Render
     return (
         <>
@@ -44,13 +52,18 @@ function SupplyInfoComponent({supply}) {
                             <span className="float-right">{dateToString(supply.creation)}</span>
                         </li>
                         <li className="list-group-item">
-                            <b>Agent/Ressource</b>
+                            <b>{(item.agent?.reference === AGENT_TYPE) ? "Agent" : "Ressource"}</b>
                             <span className="float-right">
-                                {supply.agent.name}
-                                <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
-                                   onClick={() => setAgentDetailsModal({...agentDetailsModal, show: true, id: supply.agent.id})}
-                                />
-                            </span>
+                                                {item.agent.name}
+                                {(item.agent?.reference === AGENT_TYPE)
+                                    ? <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
+                                         onClick={() => setAgentDetailsModal({...agentDetailsModal, show: true, id: item.agent.id})}
+                                    />
+                                    : <i className="fa fa-question-circle small ml-1 hand-cursor text-theme"
+                                         onClick={() => setResourceDetailsModal({...resourceDetailsModal, show: true, id: item.agent.id})}
+                                    />
+                                }
+                                            </span>
                         </li>
                         <li className="list-group-item">
                             <b>Compte émetteur</b>
@@ -101,6 +114,9 @@ function SupplyInfoComponent({supply}) {
             </FormModalComponent>
             <FormModalComponent small={true} modal={managerDetailsModal} handleClose={handleManagerDetailsModalHide}>
                 <ManagerDetailsContainer id={managerDetailsModal.id} />
+            </FormModalComponent>
+            <FormModalComponent modal={resourceDetailsModal} handleClose={handleResourceDetailsModalHide}>
+                <ResourceDetailsContainer id={resourceDetailsModal.id} />
             </FormModalComponent>
         </>
     )
